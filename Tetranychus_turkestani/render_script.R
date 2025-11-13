f71975b40445122b24d6fd4f139f7e7c9a10c58d
Siebe-vW <142795051+Siebe-vW@users.noreply.github.com>
2025-11-13 09:30:20 +0100

    Update render_script.R

--- Tetranychus_turkestani/render_script.R
+++ Tetranychus_turkestani/render_script.R
@@ -1,10 +1,9 @@
 Genus = "Tetranychus" #Insert Genus here in quotes
 species = "turkestani" #Insert species here in quotes
-setwd("C:/Users/svwunnik/OneDrive - UGent/UGent-PC/Svwunnik/Desktop/Dokter Siebe/De papers van ikke/4_Anna_Gaelle_paper/DispNet-Temperature-Dependent-Dispersal/Tetranychus_turkestani")
 
 rmarkdown::render(
-  input = paste(Genus, "_", species, ".Rmd", sep = ""),
+  input = paste(Genus, "_", species, "/", Genus, "_", species, ".Rmd", sep = ""),
   output_dir = "Knitted_Markdowns",
-  output_file = paste(Genus, "_", species, ".docx", sep = "")
+  output_file = paste(Genus, "_", species, "/", Genus, "_", species, ".docx", sep = "")
 )
 
