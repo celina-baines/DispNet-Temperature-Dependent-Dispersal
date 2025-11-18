5c4b7043d3c5f34536281577bf4b853bcfc92393
nikkiregimbal <nicole.regimbal@mail.utoronto.ca>
2025-11-18 12:55:50 -0500

    Added more explicit directory structure in the Rmd and render_script so files sort in correct places

--- Notonecta_undulata/render_script.R
+++ Notonecta_undulata/render_script.R
@@ -1,6 +1,10 @@
 Genus = "Notonecta" #Insert Genus here in quotes
 species = "undulata" #Insert species here in quotes
 
+getwd()
+setwd("../")
+dir()
+
 rmarkdown::render(
   input = paste(Genus, "_", species, "/", Genus, "_", species, ".Rmd", sep = ""),
   output_dir = "Knitted_Markdowns",
