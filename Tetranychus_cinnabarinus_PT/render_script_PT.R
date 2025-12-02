057b114f8ddca5be8ab1afe72636529fdbd558e9
inesfragata <irfragata@gmail.com>
2025-12-02 14:42:11 +0000

    Adding Tetranychus cinnabarinus PT
    Adding folder and analyses

---
+++ Tetranychus_cinnabarinus_PT/render_script_PT.R
@@ -0,0 +1,9 @@
+Genus = "Tetranychus" #Insert Genus here in quotes
+species = "cinnabarinus_PT" #Insert species here in quotes
+
+
+rmarkdown::render(
+  input = paste(Genus, "_", "cinnabarinus", "/", Genus, "_", species, ".Rmd", sep = ""),
+  output_dir = "Knitted_Markdowns",
+  output_file = paste(Genus, "_", "cinnabarinus", "/", Genus, "_", species, ".docx", sep = "")
+)
