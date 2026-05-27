143b6c230e3862d932631d2dfbdf7877c4509274
CelinaEarthSci <celina.baines@utoronto.ca>
2026-05-27 12:22:01 -0400

    add new render script for multiple strains

---
+++ render_script_multiplestrains.R
@@ -0,0 +1,9 @@
+Genus = "Genus" #Insert Genus here in quotes
+species = "species" #Insert species here in quotes
+
+rmarkdown::render(
+  input = paste(Genus, "_", species, "/", Genus, "_", species, "_multiple_strains.Rmd", sep = ""),
+  output_dir = "Knitted_Markdowns",
+  output_file = paste(Genus, "_", species, "/", Genus, "_", species, "_multiple_strains.docx", sep = "")
+)
+
